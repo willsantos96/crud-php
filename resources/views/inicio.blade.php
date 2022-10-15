c64472302b70d817b70af1880ab2dc5578dcee8b
willsantos96 <willsantos96@gmail.com>
2022-10-15 15:35:49 -0300

    add bootstrap frontend

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,39 +1,60 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto</title>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Início</title>
 </head>
 
 <body>
 
-
-    <form action="/cadastrar-produto" method="POST">
-
-        @csrf
-        <br>
-        <label for="">Nome do produto:</label>
-        <input type="text" name="nome">
-        <br><br>
-
-        <label for="">Valor do produto:</label>
-        <input type="text" name="valor">
-        <br><br>
-
-        <label for="">Quantidade em estoque:</label>
-        <input type="text" name="estoque">
-        <br><br>
-
-        <button> Salvar produto </button>
+    <div class="container">
 
 
+        <form action="/cadastrar-produto" method="POST">
 
-    </form>
+            @csrf
+            <div class="form-group">
+                <label>Nome do produto</label>
+                <input class="form-control" type="text" name="nome">
+            </div>
+
+            <div class="form-group">
+                <label>Valor do produto</label>
+                <input class="form-control" type="text" name="valor">
+                <small class="form-text text-muted">Utulize ponto no lugar da vírgula.</small>
+            </div>
+
+            <div class="form-group">
+                <label>Quantidade em estoque</label>
+                <input class="form-control" type="text" name="estoque">
+            </div>
+
+            <button type="submit" class="btn btn-dark">Enviar</button>
+        </form> <br><br><br>
 
 
+        <div class="display-flex">
+
+            <a class="btn btn-dark" href="/lista-de-produtos" role="button">Lista de produtos</a>
+        </div>
+
+
+    </div>
+
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
